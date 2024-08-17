34bd89cd915c8405773b7d4d40832095c3f6713f
in-tents <lwormand@gmail.com>
2024-08-17 10:06:37 -0700

    Create mealsdfy.py
    Defined class Meals (name, ingredients, instructions, mood),

---
+++ mealsdfy.py
@@ -0,0 +1,29 @@
+# Hello! Thanks for checking out the MealsDFY code.
+# I'm not sure how good it will be, this is
+# my first project ever, and I'm excited
+# to show you all what I've got!
+
+# I've tried to leave lots of comments, and the variables and functions
+# should be very self explanatory.
+# _--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--
+
+# First, we begin by defining the class Meals,
+# the plan being we can just call each meal in meals
+# as an individual instance
+
+class Meals:
+  # Meals has 3 attributes, initialized and defined here
+  def __init__(self, name, ingredients, instructions)
+    self.name = name
+    self.ingredients = ingredients[]
+    self.instructions = instructions[]
+    # self.mood = mood       # This will be used to separate the themes of food, like breakfasts from snacks
+
+# I shall return later to make this actually functional
+# goals:
+# 1) display a list of meal options for a specific mood when prompted
+# 2) 
+
+# Hey in-tents, it's in-tents. You are really stoned rn, so I'm leaving this note cuz I won't remember.
+# I just realized we can group all the subclasses (like snacks and desserts) into meals (Breakfasts and Postnoons).
+# We just need to add a mood attribute.
